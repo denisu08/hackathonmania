0438f419af83d26454df0788e5daa268c0ce7bfb
teakay <wulantika@gmail.com>
2015-11-20 10:39:23 +0700

    add user model

--- Client/Mdk3/app/src/main/java/com/example/user/mdk3/model/User.java
+++ Client/Mdk3/app/src/main/java/com/example/user/mdk3/model/User.java
@@ -1,10 +1,20 @@
 package com.example.user.mdk3.model;
 
+import java.util.Date;
+
 /**
  * Created by User on 11/19/2015.
  */
 public class User {
     public static String nik;
+    public static String nama;
+    public static String tempat;
+    public static Date tanggalLahir;
+    public static String gender;
+    public static String alamat;
+    public static String rtrw;
+    public static String kelurahan;
+    public static String kecamatan;
 
     public static void setNik(String nik) {
         User.nik = nik;
